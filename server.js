c1532c770d9d930a0cb2a8ae010fffd9a127f430
Ronnie95 <t_daron@yahoo.com>
2023-05-22 12:22:23 -0400

    updating API

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const express = require('express');
 const app = express();    
 const doItController = require('./controllers/doIt');
 const cors = require('cors');
+const morgan = require('morgan');
+//const { Specials } = require('./models')
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+//const { Specials } = require('./models')
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+app.use('/do', doItController);
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello</h1>`)
+    res.json('hello')
 })
+
+app.get('/*', (req, res) => {
+    res.json({comment: "This is a bad URL"});
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
